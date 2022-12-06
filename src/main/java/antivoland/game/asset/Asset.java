20be62437817dfa6d68f8a114f38c60102751776
Vladimir Pavlovskii <antivoland@gmail.com>
2022-12-06 18:37:35 +0200

    more assets

---
+++ src/main/java/antivoland/game/asset/Asset.java
@@ -0,0 +1,104 @@
+package antivoland.game.asset;
+
+// https://www.williamrobertson.net/documents/ascii.shtml
+//   https://everything2.com/title/HTML%2520symbol%2520reference
+public interface Asset {
+    char AVATAR_1 = '☹';
+    char AVATAR_2 = '☺';
+    char AVATAR_3 = '☻';
+
+    char HEART_1_FULL = '♥';
+    char HEART_1_EMPTY = '♡';
+    char HEART_2 = '❤';
+
+    char RECYCLE_FULL_1 = '♳';
+    char RECYCLE_FULL_2 = '♴';
+    char RECYCLE_FULL_3 = '♵';
+    char RECYCLE_FULL_4 = '♶';
+    char RECYCLE_FULL_5 = '♷';
+    char RECYCLE_FULL_6 = '♸';
+    char RECYCLE_FULL_7 = '♹';
+    char RECYCLE_EMPTY = '♺';
+
+    char RECYCLE_BOLD_FULL = '♻';
+    char RECYCLE_BOLD_EMPTY = '♲';
+
+    char CHECK_MARK_1 = '✓';
+    char CHECK_MARK_2 = '✔';
+    char CROSS_MARK = '✕';
+
+    String HAPPY_SQUARE = "【ツ】";
+    String HOUSE = "__̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l̡̡̡̡.___";
+    String BOOM_BOX = "♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪";
+    String CRAYONS = "((̲̅ ̲̅(̲̅C̲̅r̲̅a̲̅y̲̅o̲̅l̲̲̅̅a̲̅( ̲̅((>";
+    String FISH = "><(((('>"; // radio antenna
+    String WAVES = "°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸";
+    String SLEEPING_BABY_ON_A_PILLOW = "[{-_-}] ZZZzz zz z...";
+    String MESSAGE_1 = "(¯`·._.·(¯`·._.· Your Text ·._.·´¯)·._.·´¯)"; // jumping
+    String MESSAGE_2 = ",.-~*´¨¯¨`*·~-.¸-(_Your_Text_)-,.-~*´¨¯¨`*·~-.¸"; // jumping
+    String FISH_SWIMMING = "¸.·´¯`·.´¯`·.¸¸.·´¯`·.¸><(((º>"; // jumping
+    String CAT = "=^..^=";
+    String PROFESSOR = "'''⌐(ಠ۾ಠ)¬'''";
+    String AIRPLANE = " ‛¯¯٭٭¯¯(▫▫)¯¯٭٭¯¯’"; // vertical spaceship landing
+    String CAR_RACE = "∙،°.  ˘Ô≈ôﺣ   » » »";
+    String BENDER_ROBOT = " ¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦";
+    String CAT_2 = "龴ↀ◡ↀ龴"; // windows
+    String FACE = "•|龴◡龴|•"; // vertical wall
+    String BIG_NOSE = "˚∆˚"; // command center
+    String BIG_EYES = "⺌∅‿∅⺌"; // planets
+    String SUNNY_DAY = "☁ ▅▒░☼‿☼░▒▅ ☁";
+    String WOMAN = "▓⚗_⚗▓";
+    String EYES = "℃ↂ_ↂↃ";
+    String CAT_FACE = "⦿⽘⦿";
+    String CUTE_CAT = "^⨀ᴥ⨀^";
+    String LOVE_YOU = "ℓ٥ﻻ ﻉ√٥υ";
+    String TABLE_FLIP = "(╯°□°）╯︵ ┻━┻";
+    String MIS_MUJERES = "(-(-_(-_-)_-)-)";
+    String ONE_HUNDRED_DOLLARS = "[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]";
+    String LINE_BRACK = "●▬▬▬▬๑۩۩๑▬▬▬▬▬●";
+    String MED = "ب_ب";
+    String BULLSHIT = "|3ᵕᶦᶦᶳᶣᶨᶵ"; // thoughts
+    char FACE_HAPPY = '㋡';
+    String GUN = "︻╦╤──────"; // laser
+    String ROCKET = "∙∙∙∙∙·▫▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ☼)===>";
+    String CIGARETTE = "(____((____________()~~~"; // tunnels
+    String GUITAR = "c====(=#O| ) ~~ ♬·¯·♩¸¸♪·¯·♫¸"; // sound
+    String GHOST = "‹’’›(Ͼ˳Ͽ)‹’’›"; // sound
+    String DEATH_STAR_DEFENCE_TEAM = "|-o-| (-o-) |-o-|";
+    String TEXT_DECORATION = "(¯`·._.··¸.-~*´¨¯¨`*·~-.,-(_Your text here_)-,.-~*´¨¯¨`*·~-.¸··._.·´¯)";
+    String LOTS_OF_PPL_WAIVING = "٩(̾●̮̮̃̾•̃̾)۶٩(̾●̮̮̃̾•̃̾)۶٩(̾●̮̮̃̾•̃̾)۶٩(̾●̮̮̃̾•̃̾)۶٩(̾●̮̮̃̾•̃̾)۶٩(̾●̮̮̃̾•̃̾)۶";
+    String UP = "(◔/‿\\◔)";
+    String ROBOT_BOY = "◖(◣☩◢)◗";
+    String MED_MAN = "(⋗_⋖)";
+    String ANGRY = "ლ(ಠ益ಠ)ლ";
+    char AIRPLANE_ICON = '✈';
+    String WHAT = "ة_ة";
+    String DALEK = " ̵̄/͇̐\\ ";
+    String TABLE_FLIPPING_GUY = "(╯°□°）╯ ︵ ┻━┻"; // door
+    String ZABLE = "ಠ_ರೃ";
+    String DECORATE = "▂▃▅▇█▓▒░۩۞۩        ۩۞۩░▒▓█▇▅▃▂";
+    String SKY_FREE = "ѧѦ ѧ  ︵͡︵  ̢ ̱ ̧̱ι̵̱̊ι̶̨̱ ̶̱   ︵ Ѧѧ  ︵͡ ︵   ѧ Ѧ    ̵̗̊o̵̖  ︵  ѦѦ ѧ "; // landscape
+    String CHESS = "♞▀▄▀▄♝▀▄ ";
+    String BARBELL = "▐━━━━━▌";
+    String BEACH_BUGALOW = "|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l̡ ̴̡ı̴̴̡ ̡l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ |";
+    String LOVE_YOU_2 = "ᵛᵉᵧₒᵤᶫᵒᵛᵉᵧₒᵤ  ᶫᵒᵛᵉᵧₒᵤᶫᵒᵛᵉᵧₒᵤ  ᶫᵒᵛᵉᵧₒᵤᶫᵒᵛᵉᵧₒᵤ  ᶫᵒᵛᵉᵧₒᵤᶫᵒᵛᵉᵧₒᵤ  ᶫᵒᵛᵉᵧₒᵤᶫᵒᵛᵉᵧₒᵤ  ᶫᵒᵛᵉᵧₒᵤᶫᵒᵛᵉᵧₒᵤ  ᶫᵒᵛᵉᵧₒᵤᶫᵒᵛᵉᵧₒᵤ  ";
+    char SCISSORS = '✄';
+    String JOKER = "╭∩╮ (òÓ,) ╭∩╮"; // terrain
+    String KYUBEY = "／人 ◕‿‿◕ 人＼"; // terrain
+    String BREAKYS_ROSE = "இڿڰۣ-ڰۣ—"; // terrain
+    String DOMINO = "[: :|:::]"; // terrain
+    String OH_HI = "┬┴┬┴┤･ω･)ﾉ├┬┴┬┴";
+    String SQUID = "くコ:彡";
+    String FUCK_YOU = "ᶠᶸᶜᵏ♥ᵧₒᵤ";
+    String YOLO = "Yᵒᵘ Oᶰˡʸ Lᶤᵛᵉ Oᶰᶜᵉ";
+    String CHESS_PIECES = "♚ ♛ ♜ ♝ ♞ ♟ ♔ ♕ ♖ ♗ ♘ ♙";
+    String STARS = "⋆ ✢ ✣ ✤ ✥ ✦ ✧ ✩ ✪ ✫ ✬ ✭ ✮ ✯ ✰ ★";
+    String PAIR_OF_GLASSES = "ᒡ◯ᵔ◯ᒢ";
+
+    String INDICATOR = "oO0OoO0OoO0Oo";
+
+    // https://www.asciiart.eu/space/planets
+    // https://www.asciiart.eu/space/stars
+    // https://www.asciiart.eu/space/spaceships
+    // https://www.asciiart.eu/space/other - star sky, landing area, map
+}
