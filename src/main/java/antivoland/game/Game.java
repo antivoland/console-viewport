05a5cbbb5c791bfa9a1ed6c2f12c2c10359b0dd8
Vladimir Pavlovskii <antivoland@gmail.com>
2022-12-10 23:52:51 +0200

    cleanup

--- src/main/java/antivoland/game/Game.java
+++ src/main/java/antivoland/game/Game.java
@@ -6,13 +6,10 @@ import antivoland.console.viewport.stage.Stage;
 import antivoland.console.viewport.story.Story;
 import antivoland.console.viewport.view.component.Animation;
 import antivoland.console.viewport.view.component.Token;
-import antivoland.console.viewport.view.export.GifExporter;
 import antivoland.game.asset.Asset;
-import antivoland.game.model.Actor;
 import antivoland.game.model.Door;
 import antivoland.game.model.Player;
 
-import java.nio.file.Paths;
 import java.util.List;
 
 public class Game {
@@ -27,24 +24,24 @@ public class Game {
                     new Frame(Asset.FACE_1_HAPPY + "", 500)
             ));
 
-            var actors = List.of(
-                    player,
-                    new Actor(new Token(Asset.FACE_2)),
-                    new Actor(new Animation(
+            var views = List.of(
+                    player.view,
+                    new Token(Asset.FACE_2),
+                    new Animation(
                             new Frame(Asset.FACE_1_HAPPY + "", 200),
-                            new Frame(Asset.FACE_1_HAPPY_INVERTED + "", 200))),
-                    new Actor(new Animation(
+                            new Frame(Asset.FACE_1_HAPPY_INVERTED + "", 200)),
+                    new Animation(Frames.parse("°º¤ø,¸¸,ø¤º°`")),
+                    new Animation(
                             new Frame(Asset.FACE_1_HAPPY + "", 1000),
-                            new Frame(Asset.FACE_1_SAD + "", 1000))),
-//                    new Actor(new Animation(Frames.parse("°º¤ø,¸¸,ø¤º°`"))),
-                    new Actor(new Animation(Frames.parse("¯`·._.·`"))),
-                    new Actor(new Animation(Frames.parse("¯`·.¸¸.·´"))),
-                    new Actor(new Animation(Frames.parse("✢✣✤✥✤✣✢✢✢✢✢✢✢"))),
-                    new Actor(new Animation(Frames.parse("✦✧"))),
-                    new Actor(new Animation(Frames.parse("✩✪✫"))),
-                    new Actor(new Animation(Frames.parse("✩✪✫✬✭✮✯✰★"))),
-                    new Actor(new Token("🐩")),
-                    new Actor(new Animation(1000,
+                            new Frame(Asset.FACE_1_SAD + "", 1000)),
+                    new Animation(Frames.parse("¯`·._.·`")),
+                    new Animation(Frames.parse("¯`·.¸¸.·´")),
+                    new Animation(Frames.parse("✢✣✤✥✤✣✢✢✢✢✢✢✢")),
+                    new Animation(Frames.parse("✦✧")),
+                    new Animation(Frames.parse("✩✪✫")),
+                    new Animation(Frames.parse("✩✪✫✬✭✮✯✰★")),
+                    new Token("🐩"),
+                    new Animation(1000,
                             "♔",
                             "♕",
                             "♖",
@@ -56,36 +53,27 @@ public class Game {
                             "♜",
                             "♝",
                             "♞",
-                            "♟")
+                            "♟"
                     )
-
             );
-            var actorX = 0;
-            for (Actor actor : actors) {
-                stage.views.add(actor.view);
-                actor.view.x = actorX;
-                actorX += 3;
+            var viewX = 0;
+            for (var view : views) {
+                stage.views.add(view);
+                view.x = viewX;
+                viewX += 3;
             }
 
             var door = new Door();
             stage.views.add(door.view);
-            door.view.x = actorX + 4;
+            door.view.x = viewX + 4;
 
             // todo: open the door and reveal the next room
 
-            stage.play();
-
-            player.view.x = 0;
+            player.view.x = 30;
             player.velocity = 2.0;
-            player.move(20);
+            player.move(50);
 
-//            Thread.sleep(1000);
-//            stage.play();
-
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                stage.pause();
-                GifExporter.export(stage.recorder.frames, Paths.get("static/game.gif"));
-            }));
+            stage.play();
         }
     }
 }
